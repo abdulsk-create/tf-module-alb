776085453deb9f994f6b31b2434812eb4d8bb91c
abdul <abdulsk054@gmail.com>
2023-08-30 12:15:25 +0530

    terraform

--- main.tf
+++ main.tf
@@ -3,19 +3,8 @@ resource "aws_lb" "main" {
   internal           = var.internal
   load_balancer_type = var.lb_type
   security_groups    = [aws_security_group.main.id]
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
-
-  enable_deletion_protection = true
-
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.id
-    prefix  = "test-lb"
-    enabled = true
-  }
-
-  tags = {
-    Environment = "production"
-  }
+  subnets            = var.subnets
+  tags = merge(local.tags, {Name = "${var.env}-alb"})
 }
 
 
